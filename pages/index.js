3439bf0ecc05ca3d03b76f50dea361e76afe011e
Matsunaga syun <terada.dante@gmail.com>
2021-01-21 21:43:21 +0900

    :wastebasket: Update index.js with [profile].js
    Still don't get it

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
-const Index = () => (
-    <div>
-      <p>index page</p>
-    </div>
-  );
-  
+import Link from "next/link";
+import Layout from '../layouts/Layout'
+import fetch from 'isomorphic-unfetch'
+
+const ProfileLink = ({path, name}) => (
+  <div>
+    <Link href="/[profile]" as= {path}>
+      <a>Go to {name}'s profile</a>
+    </Link>
+  </div>
+);
+
+const Index = (props) => {
+   return ( 
+   <Layout>
+      index page
+      {props.users.map((user)=> <ProfileLink key ={user.id} name= {user.name} path = {user.path}/>)}
+    </Layout>
+    )
+   
+}
+Index.getInitialProps = async function (){
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {users : data.map((v)=> {return {
+    id : v.id,
+    name : v.name,
+    path : v.username
+  }})}
+}
 export default Index;
