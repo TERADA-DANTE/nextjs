24bd542762a6a120e47c7916f9162157b5d7ec09
Matsunaga syun <terada.dante@gmail.com>
2021-01-25 22:55:35 +0900

    :sparkles: Update inline-style of Top.js

--- my-app/src/components/Top.js
+++ my-app/src/components/Top.js
@@ -1,10 +1,21 @@
 import { Header } from 'semantic-ui-react';
+import Gnb from './Gnb';
 
 export default function Top() {
   return (
-    <>
-      <img src="/images/pig.png" alt="site-logo" />
-      <Header as="h1">Good day to live!</Header>
-    </>
+    <div>
+      <div style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column',
+      }}
+      >
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <img src="/images/pig.png" alt="site-logo" />
+          <Header as="h1">HI! TERADA</Header>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Gnb />
+        </div>
+      </div>
+    </div>
   );
 }
