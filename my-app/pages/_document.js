00e102fc78ca2a1e8fbbae76d8658ff08cd11f71
Matsunaga syun <terada.dante@gmail.com>
2021-01-25 23:04:00 +0900

    :package: Install axios

--- my-app/pages/_document.js
+++ my-app/pages/_document.js
@@ -3,14 +3,9 @@ import Document, {
 } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
-      <Html>
+      <Html lang="ja">
         <Head />
         <body>
           <Main />
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,6 +1,17 @@
+import Head from 'next/head';
 import React from 'react';
 
 export default function Home() {
-  return (<div>hello world</div>
+  const API = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+  function getData() {
+
+  }
+  return (
+    <div>
+      <Head>
+        <title>HOME | TERADA</title>
+      </Head>
+      hello world
+    </div>
   );
 }
