62b8ae513048cc6e43f389dfdb5092a00be31596
Matsunaga syun <terada.dante@gmail.com>
2021-01-26 21:17:30 +0900

    :sparkles: Update Index.js and [id].js with loading

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,30 +1,45 @@
 import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Divider, Header } from 'semantic-ui-react';
+import { Divider, Header, Loader } from 'semantic-ui-react';
 import ItemList from '../src/components/ItemList';
 
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const API = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
   function getData() {
     axios.get(API).then((res) => setList(res.data));
   }
   useEffect(() => {
     getData();
+    setIsLoading(false);
+    // setTimeout(() => setIsLoading(false), 1000);
   }, []);
   return (
-    <div>
-      <Head>
-        <title>HOME | TERADA</title>
-      </Head>
-      <Header as="h3">Best Items</Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3">New Items</Header>
-      <Divider />
-      <ItemList list={list.slice(9, 18)} />
-
-    </div>
+    <>
+      {isLoading ? (
+        <div style={{ padding: '300px 0' }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )
+        : (
+          <div>
+            <Head>
+              <title>HOME | TERADA</title>
+            </Head>
+            <Header as="h3">Best Items</Header>
+            <Divider />
+            <ItemList list={list.slice(0, 9)} />
+            <Header as="h3">New Items</Header>
+            <Divider />
+            <ItemList list={list.slice(9, 18)} />
+          </div>
+        )}
+      )
+    </>
   );
 }
--- my-app/pages/view/[id].js
+++ my-app/pages/view/[id].js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { Loader } from 'semantic-ui-react';
 import Item from '../../src/components/Item';
 
 const Post = () => {
   const [item, setItem] = useState({});
   const router = useRouter();
   const { id } = router.query;
-
+  const [isLoading, setIsLoading] = useState(true);
   const API = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
   function getData() {
     axios.get(API).then((res) => setItem(res.data));
+    setIsLoading(false);
   }
   useEffect(() => {
     if (id && id > 0) {
@@ -18,7 +20,14 @@ const Post = () => {
     }
   }, [id]);
   return (
-    <Item item={item} />
+    <>
+      {isLoading ? (
+        <Loader inline="centered" active>
+          Loading
+        </Loader>
+      )
+        : (!isLoading && <Item item={item} />)}
+    </>
   );
 };
 
