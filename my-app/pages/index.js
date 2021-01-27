48e86b83186c16452301ead540c00fb2638a529f
Matsunaga syun <terada.dante@gmail.com>
2021-01-27 18:06:06 +0900

    :sparkles: Update index.js with static-render

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,50 +1,39 @@
 import Head from 'next/head';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import {
-  Divider, Header, Dimmer, Loader, Image, Segment,
+  Divider, Header,
 } from 'semantic-ui-react';
 import ItemList from '../src/components/ItemList';
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const API = process.env.NEXT_PUBLIC_API_URL;
-  function getData() {
-    axios.get(API).then((res) => setList(res.data));
-  }
-  useEffect(() => {
-    getData();
-
-    setTimeout(() => setIsLoading(false), 500);
-  }, []);
+export default function Home({ list }) {
   return (
     <>
-      {isLoading ? (
-        <Segment>
-          <Dimmer active>
-            <Loader />
-          </Dimmer>
 
-          <Image src="/images/wireframe/short-paragraph.png" />
-        </Segment>
-      )
-        : (
-          <div>
-            <Head>
-              <title>HOME | TERADA</title>
-              <meta name="description" content="My private nextjs tutorial" />
-            </Head>
-            <Header as="h3">Best Items</Header>
-            <Divider />
-            <ItemList list={list.slice(0, 9)} />
-            <Header as="h3">New Items</Header>
-            <Divider />
-            <ItemList list={list.slice(9, 18)} />
-          </div>
-        )}
-      )
+      <div>
+        <Head>
+          <title>HOME | TERADA</title>
+          <meta name="description" content="My private nextjs tutorial" />
+        </Head>
+        <Header as="h3">Best Items</Header>
+        <Divider />
+        <ItemList list={list.slice(0, 9)} />
+        <Header as="h3">New Items</Header>
+        <Divider />
+        <ItemList list={list.slice(9, 18)} />
+      </div>
+
     </>
   );
+}
+
+export async function getStaticProps() {
+  const { API_URL } = process.env;
+  const res = await axios.get(API_URL);
+  const { data } = res;
+  return {
+    props: {
+      list: data,
+    },
+  };
 }
