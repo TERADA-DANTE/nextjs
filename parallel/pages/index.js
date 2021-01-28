db3c8d84980156b801370522b7c91aead3af97c5
Matsunaga syun <terada.dante@gmail.com>
2021-01-28 21:00:23 +0900

    :tada: Initial commit

---
+++ parallel/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "../layouts/Layout";
+
+export default function Home() {
+  return (
+    <Layout title="NextJS Pokemon">
+      <h1 className>NextJS Pokemon</h1>
+    </Layout>
+  );
+}
