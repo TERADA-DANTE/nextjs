5a2117ccb317051271274abef3f338f72e391fed
Matsunaga syun <terada.dante@gmail.com>
2021-01-28 20:58:13 +0900

    :tada: Complete my-app

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,39 +1,22 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import {
   Divider, Header,
 } from 'semantic-ui-react';
 import ItemList from '../src/components/ItemList';
 
-export default function Home({ list }) {
+export default function Home() {
+  const { NEXT_PUBLIC_NAME } = process.env;
+  console.log(NEXT_PUBLIC_NAME);
+
   return (
     <>
 
       <div>
-        <Head>
-          <title>HOME | TERADA</title>
-          <meta name="description" content="My private nextjs tutorial" />
-        </Head>
-        <Header as="h3">Best Items</Header>
-        <Divider />
-        <ItemList list={list.slice(0, 9)} />
-        <Header as="h3">New Items</Header>
-        <Divider />
-        <ItemList list={list.slice(9, 18)} />
+        w
       </div>
 
     </>
   );
-}
-
-export async function getStaticProps() {
-  const { API_URL } = process.env;
-  const res = await axios.get(API_URL);
-  const { data } = res;
-  return {
-    props: {
-      list: data,
-    },
-  };
 }
