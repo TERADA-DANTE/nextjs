42b0cdcb61cbcefed79de082184f2a9ad38d5dbb
Matsunaga syun <terada.dante@gmail.com>
2021-01-31 16:28:58 +0900

    :package: [NPM] Install styled-components

--- corona/pages/_app.js
+++ corona/pages/_app.js
@@ -2,7 +2,12 @@ import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Component
+      {...pageProps}
+      className="flex flex-col items-center justify-center"
+    />
+  );
 }
 
 export default MyApp;
--- corona/pages/index.js
+++ corona/pages/index.js
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  width: 390px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export default function Home() {
-  return <div>3</div>;
+  return (
+    <>
+      <Header>1 2 3</Header>
+    </>
+  );
 }
