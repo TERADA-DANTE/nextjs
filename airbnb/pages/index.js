350953d419ac07570f4ec729780b943eaa043917
Matsunaga syun <terada.dante@gmail.com>
2021-02-15 09:44:39 +0900

    :fire: Reset project

--- airbnb/pages/index.js
+++ airbnb/pages/index.js
@@ -1,11 +1,3 @@
-import Header from "../src/Header";
-import Hero from "../src/Hero";
-
 export default function Home() {
-  return (
-    <>
-      <Header>Hi</Header>
-      <Hero />
-    </>
-  );
+  return <></>;
 }
